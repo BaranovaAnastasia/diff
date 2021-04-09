2ac3e0b7a926f614b79cf4cb3362509378763469
Анастасия Баранова <aabaranova_3@edu.hse.ru>
2021-04-09 14:58:30 +0300

    made the 3rd param optional

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import comparison.TextsComparator;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Main class of texts comparator program.
@@ -22,16 +23,20 @@ public class Main {
 
         TextsComparator tc = new TextsComparator(Path.of(args[0]), Path.of(args[1]));
 
-        tc.saveDifferenceAsHTML(Path.of(args[2]));
+        Path outPath = args.length < 3 ? Path.of("diff.html") : Path.of(args[2]);
+        tc.saveDifferenceAsHTML(outPath);
+        System.out.println("The result is at: " + outPath.toAbsolutePath());
     }
 
     /**
-     * Checks if passed array of string contains 3 valid paths to 2 input and one output files.
+     * Checks if passed array of string contains at least 2 valid paths to the input files
+     * and, if 3rd path is present, it is a path to a .html output file.
      */
     private static boolean checkPaths(String[] paths) {
-        if (paths == null || paths.length < 3) {
+        if (paths == null || paths.length < 2) {
             System.out.println("Not enough input arguments. " +
-                    "Please provide 3 paths: to an old text, to a new one, and output .HTML file.");
+                    "Please provide at least 2 paths: to an old text, to a new one." +
+                    "Optionally, provide an output .HTML file.");
             return false;
         }
 
@@ -43,7 +48,7 @@ public class Main {
             System.out.println("File at \"" + paths[1] + "\" not found.");
             return false;
         }
-        if (!paths[2].toLowerCase().endsWith(".html")) {
+        if (paths.length >= 3 && !paths[2].toLowerCase().endsWith(".html")) {
             System.out.println("Please, provide .HTML file as an output file.");
             return false;
         }
